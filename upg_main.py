29cd35dc27d6be40b20cf3d0fa6d17511edf7019
Lukas Hagman <luha@csc.kth.se>
2015-03-25 15:00:09 +0100

    basic cmd parsing, basic gameinfo

--- upg_main.py
+++ upg_main.py
@@ -1,12 +1,18 @@
-import scripty
+import scripty, re
 
 class Room:
-	def __init__(self):
+	def __init__(self, b = None):
 		self.name = ""
 		self.ident = ""
 		self.description = ""
 		self.exits = {}
 		self.items = []
+
+		if b != None:
+			self.populateFromScripty(b)
+
+	def __str__(self):
+		return self.ident + ", " + self.name
 
 	def populateFromScripty(self, block):
 		self.name = block.getKvp("name").value
@@ -14,12 +20,11 @@ class Room:
 		self.ident = block.getKvp("id").value
 
 		exits = block.getBlock("exits")
-		print(exits)
 		if exits != None:
 			for kvp in exits.kvps:
 				self.exits[kvp.key] = kvp.value
 
-class game():
+class Game():
 	def __init__(self):
 		#alises, list of lists of commands that should be considered the same
 		#e.g., "w" and "west", "x" and "examine", usw
@@ -29,17 +34,37 @@ class game():
 		self.aliases.append(["n", "north"])
 		self.aliases.append(["s", "south"])
 
+		self.playerLocation = None
+
+def parseCommand(command):
+	command = command.strip()
+	command = re.sub(" +", " ", command)
+	print("<", command, ">", sep="")
+	return command
+
 def main():
 	scr = scripty.loadScript('sud_scr.sc')
-	scripty.printBlock(scr)
+	#scripty.printBlock(scr)
 	#for b in scr.getBlock("room"):
 		#print(b.getKvp("name").value)
 
-	r = Room()
-	r.populateFromScripty(scr.getBlock("room")[0])
-	print(r.name)
-	print(r.ident)
-	print(r.description)
-	print(r.exits)
+	rooms = [Room(b) for b in scr.getBlock("room")]
+	rd = {}
+	for room in rooms:
+		print(room.name)
+		print(room.ident)
+		print(room.description)
+		print(room.exits)
+		print()
+		rd[room.ident] = room
+	print(rd)
+
+	gameinfo = scr.getBlock("gameinfo")
+
+	g = Game()
+	g.playerLocation = rd[gameinfo.getKvp("start").value]
+	print(g.playerLocation)
+
+	print(parseCommand("   foo  bar "))
 
 main()
